2aa6bac6c3ae158bd83301c5202a6981441b1f52
Curtis Blackwell <blackwell.curtis@gmail.com>
2015-08-26 14:46:22 +0900

    :stars: copy missing content from previous release to new release

--- mr_pig/scripts/deploybot/post-upload.sh
+++ mr_pig/scripts/deploybot/post-upload.sh
@@ -9,10 +9,21 @@
 
 
 
-
-# Set the proper permissions.
+# Some needed vars.
+# The working dir at this point is `releases/$RELEASE`. This looks in `releases`, lists the last two
+# dirs (two most recent releases), and snags the first one (penultimate release).
+PREV_RELEASE=$(ls .. | tail -n 2 | head -n 1)
 SITE_DIR="html"
 
+
+# Copy old users, _content, _storage, and assets.
+rsync -avv ../$PREV_RELEASE/$SITE_DIR/_config/users $SITE_DIR/_config
+rsync -avv ../$PREV_RELEASE/$SITE_DIR/_content $SITE_DIR
+rsync -avv ../$PREV_RELEASE/$SITE_DIR/_storage $SITE_DIR
+rsync -avv ../$PREV_RELEASE/$SITE_DIR/assets $SITE_DIR
+
+
+# Set the proper permissions.
 chmod -R 777 $SITE_DIR/_cache
 chmod -R 777 $SITE_DIR/_content
 chmod -R 777 $SITE_DIR/assets
